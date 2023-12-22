b9582dac2b91009527ca2791d5deb78854efc5be
Manju <nkmanju412@gmail.com>
2023-12-22 23:51:14 +0530

    docker build file modified

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String imagename, String ImageTag, String hubUser){
     
      sh """
-      docker image build -t ${hubUser}/${project} . 
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker image build -t ${hubUser}/${imagename} . 
+      docker image tag ${hubUser}/${imagename} ${hubUser}/${imagename}:${ImageTag}
+      docker image tag ${hubUser}/${imagename} ${hubUser}/${imagename}:latest
      """
  }
 
